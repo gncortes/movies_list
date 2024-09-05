f4d4d164663ce75bb1400eb5599480930a668b2c
Gabriel Novais Cortes <novaiscortesgabriel729@gmail.com>
2024-09-05 10:21:33 -0300

    feat: create controller options

--- lib/presentation/views/movies/movies_controller.dart
+++ lib/presentation/views/movies/movies_controller.dart
@@ -1,7 +1,43 @@
+import 'package:flutter/material.dart';
+
 import '../../../data/datasource/movies_datasource.dart';
+import 'movies_notifier.dart';
 
 class MoviesController {
   final IMoviesDatasource _datasource;
 
   MoviesController(this._datasource);
+
+  final MoviesNotifier moviesNotifier = MoviesNotifier();
+  final ValueNotifier<int> _currentPageNotifier = ValueNotifier(1);
+  final ValueNotifier<bool> winnerFilterNotifier = ValueNotifier(false);
+
+  int get currentPage => _currentPageNotifier.value;
+
+  void getMoviesByYear(String year) {
+    moviesNotifier.fetch(
+      _datasource.getMoviesByYearPagined,
+      year,
+      _currentPageNotifier.value,
+      winnerFilterNotifier.value,
+    );
+  }
+
+  void nextPage(String year) {
+    _currentPageNotifier.value++;
+    getMoviesByYear(year);
+  }
+
+  void previousPage(String year) {
+    if (_currentPageNotifier.value > 1) {
+      _currentPageNotifier.value--;
+      getMoviesByYear(year);
+    }
+  }
+
+  void setWinnerFilter(bool isWinner) {
+    winnerFilterNotifier.value = isWinner;
+  }
+
+  ValueNotifier<int> get currentPageNotifier => _currentPageNotifier;
 }
--- lib/presentation/views/movies/movies_page.dart
+++ lib/presentation/views/movies/movies_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import '../../widgets/drawer/custom_drawer.dart';
 import 'movies_controller.dart';
+import 'movies_state.dart';
 
 class MoviesPage extends StatefulWidget {
   final MoviesController controller;
@@ -12,6 +12,15 @@ class MoviesPage extends StatefulWidget {
 }
 
 class _MoviesPageState extends State<MoviesPage> {
+  MoviesController get controller => widget.controller;
+  final TextEditingController _yearController = TextEditingController();
+
+  @override
+  void dispose() {
+    _yearController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +30,137 @@ class _MoviesPageState extends State<MoviesPage> {
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Filmes'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TextField(
+                    controller: _yearController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: 'Ano',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  flex: 2,
+                  child: ValueListenableBuilder<bool>(
+                    valueListenable: controller.winnerFilterNotifier,
+                    builder: (context, value, _) {
+                      return DropdownButtonFormField<bool>(
+                        value: value,
+                        decoration: const InputDecoration(
+                          labelText: 'Vencedor',
+                          border: OutlineInputBorder(),
+                        ),
+                        items: const [
+                          DropdownMenuItem(value: false, child: Text('Não')),
+                          DropdownMenuItem(value: true, child: Text('Sim')),
+                        ],
+                        onChanged: (bool? newValue) {
+                          if (newValue != null) {
+                            controller.setWinnerFilter(newValue);
+                          }
+                        },
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(width: 16),
+                SizedBox(
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      FocusScope.of(context).unfocus();
+                      if (_yearController.text.isNotEmpty) {
+                        controller.getMoviesByYear(_yearController.text);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(5),
+                          bottomRight: Radius.circular(5),
+                        ),
+                      ),
+                    ),
+                    child: const Icon(Icons.search),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ValueListenableBuilder(
+                valueListenable: controller.moviesNotifier,
+                builder: (context, value, _) {
+                  return switch (value) {
+                    MoviesLoadingState() => const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    MoviesSuccessState() => Column(
+                        children: [
+                          Expanded(
+                            child: ListView.builder(
+                              itemCount: value.movies.length,
+                              itemBuilder: (context, index) {
+                                final movie = value.movies[index];
+                                return ListTile(
+                                  title: Text(movie.title),
+                                  subtitle: Text(
+                                      'Ano: ${movie.year} | Vencedor: ${movie.winner ? "Sim" : "Não"}'),
+                                );
+                              },
+                            ),
+                          ),
+                          ValueListenableBuilder<int>(
+                            valueListenable: controller.currentPageNotifier,
+                            builder: (context, currentPage, _) {
+                              return Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  if (currentPage > 1)
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        controller.previousPage(
+                                          _yearController.text,
+                                        );
+                                      },
+                                      child: const Text('Página Anterior'),
+                                    ),
+                                  if (currentPage < value.totalPages)
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        controller.nextPage(
+                                          _yearController.text,
+                                        );
+                                      },
+                                      child: const Text('Próxima Página'),
+                                    ),
+                                ],
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    MoviesErrorState() => Center(
+                        child: Text('Erro: ${value.error.message}'),
+                      ),
+                    _ => const SizedBox.shrink(),
+                  };
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
